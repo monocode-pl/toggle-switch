200460087c991237cb7a419d345259806b6bad43
Paweł Smoleński <pwl.smolenski@gmail.com>
2019-06-11 22:00:23 +0200

    Switched from LESS to CSS

--- src/component/toggle-switch.component.js
+++ src/component/toggle-switch.component.js
@@ -1,6 +1,6 @@
 import tpl from './toggle-switch.template.html';
 
-import './toggle-switch.styles.less';
+import './toggle-switch.styles.css';
 import {ToggleSwitchController} from "./toggle-switch.controller";
 
 const ToggleSwitchComponent = {
